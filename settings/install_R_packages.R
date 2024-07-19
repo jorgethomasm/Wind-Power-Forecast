7d51a4d73dc703e9707999e425e4bb641e733d40
jorgethomasm <jorgethomasm@hotmail.com>
2024-07-19 11:34:26 +0000

    Bug fixed in installed_R_packages.R. I was thinking Python...

--- settings/install_R_packages.R
+++ settings/install_R_packages.R
@@ -3,21 +3,21 @@
 
 repo <- "https://cloud.r-project.org"
 
-r_packages <- ["BH",
-               "languageserver",
-               "httpgd",
-               "rmarkdown",
-               "knitr",
-               "reticulate",
-               "dplyr",
-               "tibble",
-               "ggplot2",
-               "plotly",
-               "lubridate",
-               "xts",
-               "kableExtra",
-               "devtools",
-               "gt"]
+r_packages <- c("BH",
+                "languageserver",
+                "httpgd",
+                "rmarkdown",
+                "knitr",
+                "reticulate",
+                "dplyr",
+                "tibble",
+                "ggplot2",
+                "plotly",
+                "lubridate",
+                "xts",
+                "kableExtra",
+                "devtools",
+                "gt")
 
 for (package in r_packages) {
 
